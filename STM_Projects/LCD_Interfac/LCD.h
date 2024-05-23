5b9223a902fe983e15ba956fa40cc17a61513dd7
Electrobrain113 <151553672+Electrobrain113@users.noreply.github.com>
2024-05-23 22:22:06 +0530

    Add files via upload

---
+++ STM_Projects/LCD_Interfac/LCD.h
@@ -0,0 +1,26 @@
+/*
+ * LCD.h
+ *
+ *  Created on: May 18, 2024
+ *      Author: karthik
+ */
+
+#ifndef SRC_LCD_H_
+#define SRC_LCD_H_
+
+
+void lcd_init (void);   // initialize lcd
+
+void lcd_send_cmd (char cmd);  // send command to the lcd
+
+void lcd_send_data (char data);  // send data to the lcd
+
+void lcd_send_string (char *str);  // send string to the lcd
+
+void lcd_put_cur(int row, int col);  // put cursor at the entered position row (0 or 1), col (0-15);
+
+void lcd_clear (void);
+
+
+
+#endif /* SRC_LCD_H_ */
